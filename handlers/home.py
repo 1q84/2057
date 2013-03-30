386ec4dc1ffc23c1e83179b9c7742e6cada58136
ericsu <ericsu1988@gmail.com>
2013-03-30 20:29:04 +0800

    like douban style

--- handlers/home.py
+++ handlers/home.py
@@ -24,6 +24,27 @@ class HomeHandler(BaseHandler):
             note.update({'user':user_map['%s'%note['author_id']]})
         self.render("timeline.html",notes=notes,user=self.get_current_user(),recent_notes=recent_notes)
 
+class NewHandler(BaseHandler):
+
+    @tornado.web.authenticated
+    def get(self, *args, **kwargs):
+        author_id = self.get_argument('author_id', None)
+        if not author_id:
+            current_user = self.get_current_user()
+            author_id=current_user['id']
+
+        users = self.service.get_friends(author_id)
+        users.append(self.get_current_user())
+        user_ids = [u['id'] for u in users]
+        user_ids = list(set(user_ids))
+        notes = self.service.batch_get_note(user_ids)
+        recent_notes = self.service.recent_notes()
+        user_map = dict((str(u['id']),u) for u in users)
+        for note in notes:
+            note.update({'user':user_map['%s'%note['author_id']]})
+        self.render("test.html",notes=notes,user=self.get_current_user(),recent_notes=recent_notes)
+
 routes = [
 	(r'/',HomeHandler),
+    (r'/new',NewHandler)
 ]
--- handlers/user.py
+++ handlers/user.py
@@ -30,16 +30,9 @@ class FansHandler(BaseHandler):
     def get(self, user_id):
         users = self.service.get_fans(user_id)
         self.render("fans.html",owner=self.service.get_user(user_id),users=users)
-
-class TestHandler(BaseHandler):
-
-    @tornado.web.authenticated
-    def get(self, *args, **kwargs):
-        self.render("test.html")
-
+        
 routes = [
 	(r'/([^/]+)/profile',ProfileHandler),
     (r'/([^/]+)/follow',FriendHandler),
     (r'/([^/]+)/fans',FansHandler),
-    (r'/test',TestHandler)
 ]
